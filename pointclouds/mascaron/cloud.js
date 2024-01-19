b78e84daad0f4810eae53ba8bf50cb01aaab0570
lacmleoni <154819118+lacmleoni@users.noreply.github.com>
2024-01-19 23:11:19 +0100

    upload pointcloud small

---
+++ pointclouds/mascaron/cloud.js
@@ -0,0 +1,91 @@
+{
+    "version": "1.8",
+    "octreeDir": "data",
+    "projection": "",
+    "points": 11421452,
+    "boundingBox": {
+        "lx": -0.14069557189941407,
+        "ly": -0.21049129962921143,
+        "lz": -0.3273754119873047,
+        "ux": 1.0627574920654297,
+        "uy": 0.9929617643356323,
+        "uz": 0.8760776519775391
+    },
+    "tightBoundingBox": {
+        "lx": -0.14069999999999895,
+        "ly": -0.21048999999999996,
+        "lz": -0.3273799999999999,
+        "ux": 1.0627600000000009,
+        "uy": 0.4691900000000002,
+        "uz": 0.4103500000000002
+    },
+    "pointAttributes": [
+        {
+            "name": "POSITION_CARTESIAN",
+            "size": 12,
+            "elements": 3,
+            "elementSize": 4,
+            "type": "int32",
+            "description": ""
+        },
+        {
+            "name": "RGBA",
+            "size": 4,
+            "elements": 4,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        },
+        {
+            "name": "number of returns",
+            "size": 1,
+            "elements": 1,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        },
+        {
+            "name": "return number",
+            "size": 1,
+            "elements": 1,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        },
+        {
+            "name": "source id",
+            "size": 2,
+            "elements": 1,
+            "elementSize": 2,
+            "type": "uint16",
+            "description": ""
+        },
+        {
+            "name": "normal__z",
+            "size": 8,
+            "elements": 1,
+            "elementSize": 8,
+            "type": "double",
+            "description": ""
+        },
+        {
+            "name": "normal__y",
+            "size": 8,
+            "elements": 1,
+            "elementSize": 8,
+            "type": "double",
+            "description": ""
+        },
+        {
+            "name": "normal__x",
+            "size": 8,
+            "elements": 1,
+            "elementSize": 8,
+            "type": "double",
+            "description": ""
+        }
+    ],
+    "spacing": 0.010422209277749062,
+    "scale": 0.001,
+    "hierarchyStepSize": 5
+}
