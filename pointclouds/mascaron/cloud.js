307cea07075ed3283494ac35da463ae5dbfac6b2
lacmleoni <154819118+lacmleoni@users.noreply.github.com>
2024-01-19 13:53:47 +0100

    add mascaron pointcloud

---
+++ pointclouds/mascaron/cloud.js
@@ -0,0 +1,91 @@
+{
+    "version": "1.8",
+    "octreeDir": "data",
+    "projection": "",
+    "points": 47932648,
+    "boundingBox": {
+        "lx": -0.08693999999999846,
+        "ly": -0.2717799999999999,
+        "lz": -0.3993899999999999,
+        "ux": 1.1388300000000005,
+        "uy": 0.953989999999999,
+        "uz": 0.826379999999999
+    },
+    "tightBoundingBox": {
+        "lx": -0.08693999999999846,
+        "ly": -0.2717799999999999,
+        "lz": -0.3993899999999999,
+        "ux": 1.1388300000000005,
+        "uy": 0.51041,
+        "uz": 0.4148900000000002
+    },
+    "pointAttributes": [
+        {
+            "name": "POSITION_CARTESIAN",
+            "size": 12,
+            "elements": 3,
+            "elementSize": 4,
+            "type": "int32",
+            "description": ""
+        },
+        {
+            "name": "RGBA",
+            "size": 4,
+            "elements": 4,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        },
+        {
+            "name": "number of returns",
+            "size": 1,
+            "elements": 1,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        },
+        {
+            "name": "return number",
+            "size": 1,
+            "elements": 1,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        },
+        {
+            "name": "source id",
+            "size": 2,
+            "elements": 1,
+            "elementSize": 2,
+            "type": "uint16",
+            "description": ""
+        },
+        {
+            "name": "normal x",
+            "size": 1,
+            "elements": 1,
+            "elementSize": 1,
+            "type": "int8",
+            "description": ""
+        },
+        {
+            "name": "normal y",
+            "size": 1,
+            "elements": 1,
+            "elementSize": 1,
+            "type": "int8",
+            "description": ""
+        },
+        {
+            "name": "normal z",
+            "size": 1,
+            "elements": 1,
+            "elementSize": 1,
+            "type": "int8",
+            "description": ""
+        }
+    ],
+    "spacing": 0.010615479201078415,
+    "scale": 0.001,
+    "hierarchyStepSize": 5
+}
